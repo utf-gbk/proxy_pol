b7a8473ced9f5222f6a4b4e76ae8c6332035a6a5
chengjiwen <chengjiwen@fangjia.com>
2018-03-23 16:06:04 +0800

    ll

--- proxies_pool/proxies_source.py
+++ proxies_pool/proxies_source.py
@@ -3,7 +3,7 @@ import json
 from .mongo_db import Mongo
 import time
 mongo = Mongo()
-url = "http://dev.kuaidaili.com/api/getproxy/?orderid=982168408884810&num=200&b_pcchrome=1&b_pcie=1&b_pcff=1&protocol=1&method=2&an_an=1&an_ha=1&sp2=1&quality=1&format=json&sep=1"
+url = "http://dev.kuaidaili.com/api/getproxy/?orderid=982168408884810&num=500&b_pcchrome=1&b_pcie=1&b_pcff=1&protocol=1&method=2&an_an=1&an_ha=1&format=json&sep=1"
 class Proxy_get():
     def get_api(self):
         while True:
@@ -12,4 +12,4 @@ class Proxy_get():
             proxy_list = ip_proxy_dict["data"]["proxy_list"]
             print(proxy_list)
             mongo.insert(proxy_list)
-            time.sleep(600)
+            time.sleep(100)
