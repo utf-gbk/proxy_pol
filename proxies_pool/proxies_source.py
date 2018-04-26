8d71c84a0f9c72828efa344706e247bdbb05b0bc
utf-gbk <32041743+utf-gbk@users.noreply.github.com>
2018-04-26 16:00:30 +0800

    Update proxies_source.py

--- proxies_pool/proxies_source.py
+++ proxies_pool/proxies_source.py
@@ -3,8 +3,8 @@ import json
 from .mongo_db import Mongo
 import time
 mongo = Mongo()
-url = "http://dev.kuaidaili.com/api/getproxy/?orderid=982168408884810&num=150&b_pcchrome=1&b_pcie=1&b_pcff=1&protocol=1&method=2&an_an=1&an_ha=1&sp2=1&f_sp=1&quality=1&format=json&sep=1"
-url_1 = "http://dev.kuaidaili.com/api/getproxy/?orderid=982168408884810&num=200&b_pcchrome=1&b_pcie=1&b_pcff=1&protocol=1&method=1&format=json&sep=1"
+url = "your ip_proxy api"
+url_1 = "your ip_proxy api"
 class Proxy_get():
     def get_api(self):
         while True:
